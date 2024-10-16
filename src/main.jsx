16b5b24a258932e41cfe4b274bc7a9c1bd163479
Dev-U-Patel <animx4522@gmail.com>
2024-10-16 19:52:17 +0530

    Last Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, HashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, HashRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import SignInPage from './auth/sign-in/SignInPage.jsx'
 import Home from './home/Home.jsx'
 import DashBoard from './dashboard/DashBoard.jsx'
@@ -12,40 +12,46 @@ import ViewResume from './my-resume/[resumeID]/view/ViewResume.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: '/dashBoard',
-        element: <DashBoard />
-      },
-      {
-        path: '/dashBoard/resume/:resumeId/edit',
-        element: <ResumeEdit />
-      }
-    ]
-  },
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/auth/sign-in',
-    element: <SignInPage />
-  },
-  {
-    path: '/my-resume/:resumeId/view',
-    element: <ViewResume />
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/dashBoard',
+//         element: <DashBoard />
+//       },
+//       {
+//         path: '/dashBoard/resume/:resumeId/edit',
+//         element: <ResumeEdit />
+//       }
+//     ]
+//   },
+//   {
+//     path: '/',
+//     element: <Home />
+//   },
+//   {
+//     path: '/auth/sign-in',
+//     element: <SignInPage />
+//   },
+//   {
+//     path: '/my-resume/:resumeId/view',
+//     element: <ViewResume />
+//   }
+// ])
 
 createRoot(document.getElementById('root')).render(
-
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-    <HashRouter>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/sign-in" element={<SignInPage />} />
+          <Route path="/dashBoard" element={<DashBoard />} />
+          <Route path="/dashBoard/resume/:resumeId/edit" element={<ResumeEdit />} />
+          <Route path="/my-resume/:resumeId/view" element={<ViewResume />} />
+        </Routes>
     </HashRouter>
-  </ClerkProvider>
+  </ClerkProvider>  
   ,
 )
